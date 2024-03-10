bd056e0f0d0faad3f271d8ef42d1136d632afd4b
AndreyUrazaev <f.a.z.a@bk.ru>
2024-03-10 11:04:25 +0500

    hone-work-02-02(01)

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -13,28 +13,20 @@ import s from './Affairs.module.css'
 type AffairsPropsType = {
   data: AffairType[] // need to fix any
   setFilter: (filter: FilterType) => void // запоминай, как типизируется useState
+  // setFilter: Dispatch<SetStateAction<FilterType>> // полноценное описание типа, с использованием lifehack "number"
   deleteAffairCallback: (_id: number) => void  //не забывай, что функция сюда приехала не пустой
   filter: FilterType
 }
 
 function Affairs(props: AffairsPropsType) {
-  // const setAll = () => {
-  //     // need to fix
-  //     //пропс.setFilter('all')
-  // }
-  // const setHigh = () => {
-  //     // need to fix
-  // }
-  // const setMiddle = () => {
-  //     // need to fix
-  // }
-  // const setLow = () => {
-  //     // need to fix
-  // }
   const setAll = () => props.setFilter("all");
   const setHigh = () => props.setFilter("high");
   const setMiddle = () => props.setFilter("middle");
   const setLow = () => props.setFilter("low");
+  // ----------------использование одной переменной
+  // const setFilter = (e: React.MouseEvent<HTMLButtonElement>) => {
+  //   props.setFilter(e.currentTarget.value as FilterType);
+  // }
   
   const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? ' ' + s.active : '')
   const cnHigh = s.button + ' ' + s.high + (props.filter === 'high' ? ' ' + s.active : '')
@@ -59,38 +51,18 @@ function Affairs(props: AffairsPropsType) {
   return (
     <div>
       <div className={s.buttonContainer}>
-        <button
-          id={'hw2-button-all'}
-          onClick={setAll}
-          className={cnAll}
-        >
-          All
-        </button>
-        <button
-          id={'hw2-button-high'}
-          onClick={setHigh}
-          className={cnHigh}
-        >
-          High
-        </button>
-        <button
-          id={'hw2-button-middle'}
-          onClick={setMiddle}
-          className={cnMiddle}
-        >
-          Middle
-        </button>
-        <button
-          id={'hw2-button-low'}
-          onClick={setLow}
-          className={cnLow}
-        >
-          Low
-        </button>
+        <button id={'hw2-button-all'} onClick={setAll} className={cnAll} value={"all"}>All</button>
+        <button id={'hw2-button-high'} onClick={setHigh} className={cnHigh} value={"all"}>High</button>
+        <button id={'hw2-button-middle'} onClick={setMiddle} className={cnMiddle} value={"all"}>Middle</button>
+        <button id={'hw2-button-low'} onClick={setLow} className={cnLow} value={"all"}>Low</button>
       </div>
-      {/*Вот то что мы мапили на стр40 здесь используем. Можно было конечно прямо*/}
-      {/*здесь мапить и разводить болото, но мы решили работать по красивому и*/}
-      {/*все вынесли*/}
+      {/*----------------использование одной переменной*/}
+      {/*<div className={s.buttonContainer}>*/}
+      {/*  <button id={'hw2-button-all'} onClick={setFilter} className={cnAll} value={"all"}> All</button>*/}
+      {/*  <button id={'hw2-button-high'} onClick={setFilter} className={cnHigh} value={"high"}> High</button>*/}
+      {/*  <button id={'hw2-button-middle'} onClick={setFilter} className={cnMiddle} value={"middle"}> Middle</button>*/}
+      {/*  <button id={'hw2-button-low'} onClick={setFilter} className={cnLow} value={"low"}> Low</button>*/}
+      {/*</div>*/}
       <div className={s.affairs}>{mappedAffairs}</div>
     </div>
   )
