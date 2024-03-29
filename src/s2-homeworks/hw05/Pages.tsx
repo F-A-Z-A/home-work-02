980bdbe4a0fbe41232acc4a17566f8ff6c3cb0db
AndreyUrazaev <f.a.z.a@bk.ru>
2024-03-29 20:01:29 +0500

    hone-work-03-05(01)

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -9,6 +9,7 @@ export const PATH = {
   PRE_JUNIOR: '/pre-junior',
   JUNIOR: '/junior',
   JUNIOR_PLUS: '/junior-plus',
+  ERROR_404: '/error404'
 }
 
 function Pages() {
@@ -26,12 +27,12 @@ function Pages() {
         {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
         {/*<Route ...*/}
         
-        <Route path='/' element={<Navigate to='/pre-junior'/>}/>
-        <Route path="/pre-junior" element={<PreJunior/>}/>
-        <Route path="/junior" element={<Junior/>}/>
-        <Route path="/junior-plus" element={<JuniorPlus/>}/>
-        <Route path="/error404" element={<Error404/>}/>
-        <Route path="/*" element={<Navigate to="/error404"/>}/>
+        <Route path='/' element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+        <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+        <Route path={PATH.JUNIOR} element={<Junior/>}/>
+        <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
+        <Route path={PATH.ERROR_404} element={<Error404/>}/>
+        <Route path="/*" element={<Navigate to={PATH.ERROR_404}/>}/>
       </Routes>
     </div>
   )
