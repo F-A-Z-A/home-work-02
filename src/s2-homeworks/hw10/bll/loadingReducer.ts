c72c66ba704d3316898950f50a8ced42da2e6f81
AndreyUrazaev <f.a.z.a@bk.ru>
2024-05-12 18:43:02 +0500

    home-work-04-10(1)

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,41 @@
-const initState = {
-    isLoading: false,
+export type IsLoadingStateType = { isLoading: boolean }
+
+const initState = {isLoading: false}
+
+export const loadingReducer = (state = initState, action: LoadingActionType): IsLoadingStateType => {
+  switch (action.type) {
+    case "CHANGE_LOADING":
+      return {...state, isLoading: action.isLoading}
+    default:
+      return state
+  }
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
+type LoadingActionType = ReturnType<typeof loadingAC>
 
-        default:
-            return state
-    }
-}
+export const loadingAC = (isLoading: boolean) =>
+  ({type: 'CHANGE_LOADING', isLoading} as const)
 
-type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
 
-export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
-})
+// const initState = {
+//     isLoading: false,
+// }
+//
+// export const loadingReducer = (state = initState, action: any): any => { // fix any
+//     switch (action.type) {
+//         // пишет студент  // need to fix
+//
+//         default:
+//             return state
+//     }
+// }
+//
+// type LoadingActionType = {
+//     type: 'CHANGE_LOADING'
+//     isLoading: boolean
+// }
+//
+// export const loadingAC = (isLoading: boolean): LoadingActionType => ({
+//     type: 'CHANGE_LOADING',
+//     isLoading,
+// })
